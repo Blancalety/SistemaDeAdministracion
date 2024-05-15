c75f169da638ed109b08313b7b8db9b83c11c051
BlancaLeti <blancalety.r@gmail.com>
2024-05-14 21:40:49 -0600

    se agrego menu de navegacion con imagen de fondo iconos y formulario a bienvenido.php

--- administrador/bienvenido.php
+++ administrador/bienvenido.php
@@ -13,26 +13,64 @@ $nombre = $_SESSION['nombreUser'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido</title>
 
-    <style>
-
-        body {
-            background-color: #f2f2f2;
-        }
-
-        .titulo {
-            font-family: 'Verdana', sans-serif;
-            font-size: 1em;
-            font-weight:bolder;
-            margin-top: 8rem;
-            margin-bottom: 20px;
-            text-align: center;
-        }
-
-    </style>
 </head>
 <body>
 
-<div class="titulo">Hola <?php echo $nombre; ?> bienvenido al sistema Star</div>
+    <header>
+
+        <h1 class="titulo"><?php echo $nombre; ?> <span>bienvenido al sistema Star </span></h1>
+
+    </header>
+    <!-- <div class="titulo">Hola <?php echo $nombre; ?> bienvenido al sistema Star</div> -->
+
+    <?php
+
+        include('navegacion.php');
+
+    ?>
+
+        <section>
+
+            <form action="http://google.com" class="formulario">
+                <fieldset>
+                    <legend>Contactame llenando todos los campos</legend>
+                    <div class="contenedor-campos">
+                        <div class="campo">
+                            <label>Nombre</label>
+                            <input class="input-text" type="input-text" placeholder="Tu nombre">
+                        </div>
+
+                        <div class="campo">
+                            <label>Telefono</label>
+                            <input class="input-text" type="input-text" placeholder="Tu telefono">
+                        </div>
+
+                        <div class="campo">
+                            <label>Correo</label>
+                            <input class="input-text" type="input-text" placeholder="Tu email">
+                        </div>
+
+                        <div class="campo">
+                            <label>Mensaje</label>
+                            <textarea class="input-text"></textarea>
+                        </div>
+                    </div><!--.contenedor-campos-->
+
+                    <div class="alinear-derecha flex">
+                            <input class="boton w100" type="submit" value="Enviar">
+                        
+                    </div>
+
+                </fieldset>
+            </form>
+        </section>
+
+    </main>
+
+
+    <footer class="footer">
+        <p>Todos los derechos reservados. Blanca leticia RR</p>
+    </footer>
 
   
 </body>
